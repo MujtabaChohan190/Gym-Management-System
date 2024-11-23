1c4252cf86a13cd8477ebd68bcede0cbe6df851b
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-11-23 15:48:41 +0500

    5th commit - Save members function

--- project.c
+++ project.c
@@ -56,6 +56,7 @@ int main() {
     return 0;
 }
 
+//Writing function to load members from file to process things
 void loadMembersFromFile() {
     FILE *file = fopen(FILE_NAME, "r");
     if (file == NULL) {
@@ -76,5 +77,19 @@ void loadMembersFromFile() {
                   &member.paymentStatus, member.feedback) != EOF) {
         members[memberCount++] = member;
     }
+    fclose(file);
+}
+
+//Writing function to save members to file
+void saveMembersToFile() {
+    FILE *file = fopen(FILE_NAME, "w");
+    if (file == NULL) {
+        printf("Error: Unable to save members to file.\n");
+        return;
+    }
+
+    for (int i = 0; i < memberCount; i++) {
+        fprintf(file, "%d,%s,%s,%d,%d,%d,%s\n", members[i].id, members[i].name, members[i].membershipType, members[i].feesPaid, members[i].attendance, members[i].paymentStatus, members[i].feedback);
+    }   //fprintf used to write formatted output to the file 
     fclose(file);
 }
