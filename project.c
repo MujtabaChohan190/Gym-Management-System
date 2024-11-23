09ef53a24fed9d2e7dbebb96d6157944a0081a6b
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-11-23 12:50:17 +0500

    3rd commit

--- project.c
+++ project.c
@@ -22,3 +22,36 @@ typedef struct {
     int assignedMembers[10]; // Max of 10 members per trainer
     int assignedCount;
 } Trainer;
+
+// Global variables
+Member members[100];
+Trainer trainers[10];
+int memberCount = 0;
+int trainerCount = 0;
+const char *FILE_NAME = "members.txt";
+
+// Function prototypes
+void loadMembersFromFile();
+void saveMembersToFile();
+void loginSystem();
+void mainMenu();
+void addMember();
+void viewMembers();
+void updateMember();
+void deleteMember();
+void searchMember();
+void assignMembersToTrainers();
+void attendanceTracking();
+void upgradeDowngradeMembership();
+void automaticFeeCalculation();
+void memberFeedback();
+void generateReports();
+void pressAnyKey();
+int findMemberIndex(int id);
+
+int main() {
+    loadMembersFromFile();
+    loginSystem();
+    mainMenu();
+    return 0;
+}
